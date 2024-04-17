b8b1499ebde9b1b6ce85f2c335c67759356c5127
jas0xf <jas0xf@gmail.com>
2024-04-17 21:51:49 +0800

    add: database Exchange

--- models/database.py
+++ models/database.py
@@ -14,3 +14,9 @@ class User(db.Model):
 
     def as_dict(self):
         return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
+
+
+class Exchange(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(80), nullable=False)
+
